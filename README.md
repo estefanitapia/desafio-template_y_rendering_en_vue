b64c998b5bc3d181fbe211d48deea12dab25116a
estefanitapia <estefani.tapia.godoy@gmail.com>
2024-07-02 22:09:53 -0400

    ultimos estilos y readme

--- README.md
+++ README.md
@@ -1,24 +1,23 @@
 # desafio-templates_y_rendering_en_vue
 
-## Project setup
-```
-npm install
-```
+![Interfaz a maquetar](./src/assets/form.png)
 
-### Compiles and hot-reloads for development
-```
-npm run serve
-```
+## Consigna
 
-### Compiles and minifies for production
-```
-npm run build
-```
+En este desafío tendremos que crear una aplicación Vue Js desde 0 y maquetar la interfaz que aparece en la imagen.  
+Los inputs del formulario deberán modificar los estilos de la figura de la derecha.
 
-### Lints and fixes files
-```
-npm run lint
-```
+### Requerimientos
 
-### Customize configuration
-See [Configuration Reference](https://cli.vuejs.org/config/).
+1. Usar el style binding para asignar estilos reactivamente a un elemento usando valores
+del estado.
+
+2. Usar el class binding para asignar clases reactivamente a un elemento usando valores
+booleanos del estado.
+
+3. Usar v-for para iterar un arreglo del estado en el template para la renderización
+dinámica de elementos.
+
+4. Usar v-show para mostrar u ocultar la figura.
+
+
--- src/components/MaquetaInterfaz.vue
+++ src/components/MaquetaInterfaz.vue
@@ -3,105 +3,160 @@
     <form class="form">
       <div>
         <span>Escribe un color de fondo en inglés</span>
-        <br>
+        <br />
         <input class="form-control ancho" v-model="colorCuadrado" />
       </div>
       <div>
         <span>Escribe un color de letra en inglés</span>
-        <br>
+        <br />
         <input class="form-control ancho" v-model="colorLetra" />
       </div>
       <div>
         <label>Mostrar texto</label>
-        <input v-model="show" type="checkbox">
+        <input v-model="show" type="checkbox" />
       </div>
       <div>
         <label for="customRange2" class="form-label ancho">Borde</label>
-        <br>
-        <input v-model="border" type="range" class="form-range ancho" :min="minimo" :max="maximo" step="0.5"
-          id="customRange2">
+        <br />
+        <input
+          v-model="border"
+          type="range"
+          class="form-range ancho"
+          :min="minimo"
+          :max="maximo"
+          step="0.5"
+          id="customRange2"
+        />
       </div>
       <div>
         <label>Contenido textual: </label>
-        <br>
-        <textarea v-model="texto"></textarea>
+        <br />
+        <textarea maxlength="200" v-model="texto"></textarea>
       </div>
-      <label>Tipografía: </label>
-      <br>
-      <select v-model="typography">
-        <option :key="tipografia" v-for="tipografia in tipografias" :value="tipografia">{{ tipografia }}</option>
-      </select>
+      <div>
+        <label>Tipografía: </label>
+        <br />
+        <select v-model="typography">
+          <option
+            :key="tipografia"
+            v-for="tipografia in tipografias"
+            :value="tipografia"
+          >
+            {{ tipografia }}
+          </option>
+        </select>
+      </div>
+
       <div>
         <label>Opaco</label>
-        <input v-model="opaco" type="checkbox">
+        <input v-model="opaco" type="checkbox" />
       </div>
       <div>
         <span>Tamaño de letra</span>
         <div class="form-check">
-              <input class="form-check-input" type="radio" name="flexRadioDefault" id="p" value="0.5rem"
-                v-model="size" />
-              <label class="form-check-label fw-normal" for="p"> Pequeño </label>
-            </div>
-            <div class="form-check">
-              <input class="form-check-input" type="radio" name="flexRadioDefault" id="m" value="1rem"
-                v-model="size" />
-              <label class="form-check-label fw-normal" for="m"> Mediano </label>
-            </div>
-            <div class="form-check">
-              <input class="form-check-input" type="radio" name="flexRadioDefault" id="g" value="5rem"
-                v-model="size" />
-              <label class="form-check-label fw-normal" for="g"> Grande </label>
-            </div>
+          <input
+            class="form-check-input"
+            type="radio"
+            name="flexRadioDefault"
+            id="p"
+            value="1rem"
+            v-model="size"
+          />
+          <label class="form-check-label fw-normal" for="p"> Pequeño </label>
+        </div>
+        <div class="form-check">
+          <input
+            class="form-check-input"
+            type="radio"
+            name="flexRadioDefault"
+            id="m"
+            value="1.5rem"
+            v-model="size"
+          />
+          <label class="form-check-label fw-normal" for="m"> Mediano </label>
+        </div>
+        <div class="form-check">
+          <input
+            class="form-check-input"
+            type="radio"
+            name="flexRadioDefault"
+            id="g"
+            value="2rem"
+            v-model="size"
+          />
+          <label class="form-check-label fw-normal" for="g"> Grande </label>
+        </div>
       </div>
     </form>
-    <div id="cuadrado"
-      :style="{ background: colorCuadrado, color: colorLetra, borderRadius: radioCompleto, fontFamily: typography, fontSize: size, }"
-      :class="{ 'opaco': opaco }">
-      <h1 v-if="show"> {{ texto }}</h1>
+    <div
+      id="cuadrado"
+      :style="{
+        background: colorCuadrado,
+        color: colorLetra,
+        borderRadius: radioCompleto,
+        fontFamily: typography,
+        fontSize: size,
+      }"
+      :class="{ opaco: opaco }"
+    >
+      <p v-if="show">{{ texto }}</p>
     </div>
-
-
   </div>
 </template>
 
 <script>
 export default {
-  name: 'ModidicarCuadro',
+  name: "ModidicarCuadro",
   data() {
     return {
       colorCuadrado: "",
       colorLetra: "",
       show: "",
       border: "",
-      minimo: "",
-      maximo: "",
+      minimo: "1",
+      maximo: "50",
       texto: "",
       typography: "",
       opaco: false,
       size: "",
-      tipografias: ["Times New Roman", "Georgia", "Arial", "Verdana", "Tahoma", "Sanvito", "Courier",],
-
+      tipografias: [
+        "Times New Roman",
+        "Georgia",
+        "Arial",
+        "Verdana",
+        "Tahoma",
+        "Sanvito",
+        "Courier",
+      ],
     };
   },
   computed: {
     radioCompleto() {
-      return this.border + "px"
+      return this.border + "%";
     },
-  }
-}
+  },
+};
 </script>
 
-
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
+<style>
+.container {
+  display: flex;
+  align-items: stretch;
+  gap: 2rem;
+}
+
 #cuadrado {
   width: 500px;
   height: 500px;
-  display: inline-block;
+  display: flex;
   align-items: center;
   justify-content: center;
   background-color: #ccc;
   margin-left: 50px;
+  overflow: auto;
+  padding: 1rem;
+  align-self: center;
 }
 
 form {
@@ -113,13 +168,19 @@ form {
   gap: 15px;
   padding: 40px;
   font-size: 16px;
+
+  & > div {
+    margin-top: 1rem;
+  }
 }
 
 .opaco {
   opacity: 0.5; /* o cualquier valor de opacidad que desees */
 }
 
-.ancho, textarea, select {
+.ancho,
+textarea,
+select {
   width: 250px;
 }
 </style>
